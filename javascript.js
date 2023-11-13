b5cf126b131ad8caecd01666690ccf6571bed117
ilathem <isaiahlathem@gmail.com>
2023-11-12 19:40:26 -0600

    Finish foundations fundamentals part 1 assignment

--- javascript.js
+++ javascript.js
@@ -15,3 +15,24 @@ console.log("Hello from javascript.js!");
 // Uppercase const?
 // const BIRTHDAY = '18.04.1982';
 // const age = someCode(birthday);
+
+
+// Fundamentals Part 1 Assignment:
+console.log("Fundamentals Part 1 Assignment:");
+console.log("1:");
+console.log(23+92);
+console.log("2:");
+console.log(1+2+3+4+5+6);
+console.log("3:");
+console.log(((4+6+9) / 77).toFixed(5));
+console.log("4:");
+let a = 10;
+console.log(a);
+a = 9 * a;
+let b = 7 * a;
+console.log(b)
+console.log("5:");
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
+console.log(percentage.toFixed(4));
