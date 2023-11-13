3b9f74087136b3a396dadc4c8edd8e3b7366a9a1
ilathem <isaiahlathem@gmail.com>
2023-11-12 19:12:55 -0600

    Finish variable tutorial

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
 console.log("Hello from javascript.js!");
+
+// Tasks from https://javascript.info/variables#tasks
+// Working with variabes:
+// let admin;
+// let name;
+// name = "John";
+// admin = name;
+// alert(admin);
+
+// Giving the right name:
+// const OUR_PLANET = Earth;
+// let currentVisitor = "John";
+
+// Uppercase const?
+// const BIRTHDAY = '18.04.1982';
+// const age = someCode(birthday);
